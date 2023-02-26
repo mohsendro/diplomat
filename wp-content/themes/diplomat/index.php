3935589df886997d290fef7577584296a5c38734
mohsendro <mohsendroo@yahoo.com>
2023-02-26 10:27:33 +0330

    one commit 1401/12/07

--- wp-content/themes/diplomat/index.php
+++ wp-content/themes/diplomat/index.php
@@ -1,12 +1,3 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
-
-?>
-
-<h1>index Theme</h1>
-<?php get_header(); ?>
-
-<?php the_content(); ?> 
-
-<?php get_footer(); ?>
