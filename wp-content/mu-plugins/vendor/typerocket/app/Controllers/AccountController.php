4c0a7d1dbe37e5e0545dc8029be225e109cc5718
Mohsendro <78999228+mohsendro@users.noreply.github.com>
2023-02-27 16:43:57 +0330

    nine commit 1401/12/08

--- wp-content/mu-plugins/vendor/typerocket/app/Controllers/AccountController.php
+++ wp-content/mu-plugins/vendor/typerocket/app/Controllers/AccountController.php
@@ -97,4 +97,27 @@ class AccountController extends Controller
         return tr_view('account.account', compact('endpoint') );
 
     }
+
+    /**
+     * The index page for admin
+     *
+     * @return mixed
+     */
+    public function update()
+    {
+
+        // $tr_request = tr_request();
+        // $to_job->user_id = $tr_request->getDataPost('tojob_add_user_id');
+        // $to_job->job_id  = $tr_request->getDataPost('tojob_add_job_id');
+        // $to_job->content = $tr_request->getDataPost('tojob_add_content');
+        // if( $tr_request->getDataPost('tojob_add_status') ) {
+        //     $to_job->status = "active";
+        // } else {
+        //     $to_job->status = "deactive";
+        // }
+        // $to_job->save(); 
+        // tr_response()->flashNext('درخواست جدید ثبت شد'); 
+        // return tr_redirect()->toPage('tojob', 'index');
+
+    }
 }
